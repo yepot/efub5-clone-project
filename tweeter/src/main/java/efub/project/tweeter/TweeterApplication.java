7e9ac21aaacb4ac62d32e8f173bd5b05ce11106f
yepot <yes03326@gmail.com>
2025-05-23 14:04:43 +0900

    chore: initial project setup

---
+++ tweeter/src/main/java/efub/project/tweeter/TweeterApplication.java
@@ -0,0 +1,13 @@
+package efub.project.tweeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterApplication.class, args);
+	}
+
+}
