89dae512c456789a6435c3a8ab15af86fbbf7e4f
yepot <yes03326@gmail.com>
2025-05-27 21:20:12 +0900

    fix: start.sh

--- tweeter/scripts/start.sh
+++ tweeter/scripts/start.sh
@@ -14,7 +14,9 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG2> $ERROR_LOG &
+cd $PROJECT_ROOT
+nohup java -jar tweeter-0.0.1-SNAPSHOT.jar > $APP_LOG 2> $ERROR_LOG < /dev/null &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스의 아이디는 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+
