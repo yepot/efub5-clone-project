7d7cd7015440fd4675a6990868839e8c61238b3c
yepot <yes03326@gmail.com>
2025-05-27 20:44:38 +0900

    fix: start.sh, stop.sh

--- tweeter/scripts/start.sh
+++ tweeter/scripts/start.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/env bashPROJECT_ROOT="/home/ubuntu/tweeter"
-
+#!/usr/bin/env bash
+PROJECT_ROOT="/home/ubuntu/tweeter"
 JAR_FILE="$PROJECT_ROOT/tweeter-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)# build 파일 복사
+TIME_NOW=$(date +%c)
+
+# build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
@@ -14,4 +16,5 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)echo "$TIME_NOW > 실행된 프로세스의 아이디는 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스의 아이디는 $CURRENT_PID 입니다." >> $DEPLOY_LOG
