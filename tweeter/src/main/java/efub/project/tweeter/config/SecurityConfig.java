de174f4f5c079e3e200fa2ae61417cdfce0ef5a2
yepot <yes03326@gmail.com>
2025-05-29 19:16:10 +0900

    feat:  front domain address

--- tweeter/src/main/java/efub/project/tweeter/config/SecurityConfig.java
+++ tweeter/src/main/java/efub/project/tweeter/config/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig {
         // 만약 "https://test.com"라는 도메인을 전달받았다면, 아래 코드에 추가로 다음과 같이 입력합니다.
         // configuration.setAllowedOrigins(Arrays.asList(..., "https://test.com"));
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000", "http://localhost:8080", "https://api.efub-tweeter.p-e.kr/"));
+                "http://localhost:5173/", "http://localhost:8080",
+                "https://api.efub-tweeter.p-e.kr/", "https://efub5-twitter-clone.vercel.app/"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
