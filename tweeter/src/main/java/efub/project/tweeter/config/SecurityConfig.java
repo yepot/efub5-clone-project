d340592a7044f2b05384b7098da6e9a3d3dd3cd0
yepot <yes03326@gmail.com>
2025-06-02 13:01:07 +0900

    feat: fix config

--- tweeter/src/main/java/efub/project/tweeter/config/SecurityConfig.java
+++ tweeter/src/main/java/efub/project/tweeter/config/SecurityConfig.java
@@ -18,17 +18,15 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    // cors 설정
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        // 프론트엔드의 도메인이 들어가는 파트입니다.
-        // 미리 짝개발자에게 이야기해, 배포된 도메인을 받아 새로 도메인을 allow 해줍니다.
-        // 만약 "https://test.com"라는 도메인을 전달받았다면, 아래 코드에 추가로 다음과 같이 입력합니다.
-        // configuration.setAllowedOrigins(Arrays.asList(..., "https://test.com"));
+        // CORS 허용 도메인 추가
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000", "https://example.vercel.app/"));
+                "http://localhost:5173",
+                "https://efub5-twitter-clone.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
@@ -37,5 +35,20 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()));
+        http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/**").permitAll() // 모든 요청 허용
+        );
+
+        return http.build();
     }
 }
