ce0cc53e847d6b7e3526debe1af1702bb05e319c
yepot <yes03326@gmail.com>
2025-06-03 22:32:49 +0900

    fix: change SecurityConfig.java into WebConfig.java

--- tweeter/src/main/java/efub/project/tweeter/config/WebConfig.java
+++ tweeter/src/main/java/efub/project/tweeter/config/WebConfig.java
@@ -6,19 +6,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class SecurityConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "*" // 임시 허용
-//                        "http://localhost:5173",
-//                        "https://efub5-twitter-clone.vercel.app"
+                        "http://localhost:5173",
+                        "https://efub5-twitter-clone.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
-//                .allowCredentials(true)
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
