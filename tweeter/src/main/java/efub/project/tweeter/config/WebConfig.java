98cb7c375dceead1f92e19bc8b9943d59911dc97
yepot <yes03326@gmail.com>
2025-06-04 15:44:54 +0900

    feat: fix /login error

--- tweeter/src/main/java/efub/project/tweeter/config/WebConfig.java
+++ tweeter/src/main/java/efub/project/tweeter/config/WebConfig.java
@@ -1,8 +1,12 @@
 package efub.project.tweeter.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,4 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true)  // 쿠키 등 credentials 허용
                 .maxAge(3600);
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
 }
